b0b72ba4e10c5fde9965d826f3ce1b537e768b86
Viktor Zenk <zevi1720@student.ju.se>
2018-10-04 13:21:04 +0200

    broke the code

--- src/se/ju/csi/oom/lab4/DateTime.java
+++ src/se/ju/csi/oom/lab4/DateTime.java
@@ -13,7 +13,7 @@ public class DateTime {
 	private int second;
 	
 	public String toString() {
-		return String.format("%04d-%02d-%02d %02d:%02d:%02d", year, month, day, hour, minute, second);
+		return String.format("%04d-%02d-%02d %02d:%02d", year, month, day, hour, minute);
 	}
 	
 	public DateTime(int year, int month, int day, int hour, int minute, int second) {
